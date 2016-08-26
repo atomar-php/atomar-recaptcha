2f2678cf68a64477c551f690063428bdcb52fe81
Joel Lonbeck <joel@neutrinographics.com>
2016-08-25 22:31:38 -0700

    initial commit

---
+++ install.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace recaptcha;
+
+/**
+ * Implements hook_uninstall()
+ */
+function uninstall() {
+    // TODO: perform uninstall operations here
+    return true;
+}
+
+/**
+ * Implements hook_update_version()
+ */
+function update_1() {
+    // TODO: perform update operations here
+    return true;
+}
